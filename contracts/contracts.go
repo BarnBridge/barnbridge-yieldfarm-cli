33fea98a8a9688fd08cf9b4055aee527cbdf8f86
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-26 17:12:38 +0800

    Delete contracts directory

--- contracts/contracts.go
+++
@@ -1,4 +0,0 @@
-//go:generate abigen --abi ../abi/Staking.json --pkg contracts --out Staking.go --type Staking
-//go:generate abigen --abi ../abi/YieldFarm.json --pkg contracts --out YieldFarm.go --type YieldFarm
-//go:generate abigen --abi ../abi/YieldFarmLP.json --pkg contracts --out YieldFarmLP.go --type YieldFarmLP
-package contracts
